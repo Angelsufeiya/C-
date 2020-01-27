06a25a80c46a1a0d6a3dc98845b5a8a06a564451
武纪东 <Angelsufeiya@hotmail.com>
2020-01-27 23:58:34 +0800

    come on

--- 每日一题/test.c
+++ 每日一题/test.c
@@ -15,13 +15,60 @@
 //15
 //Êä³ö
 //2
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 
-int main(){
+int fib(int n){
+	if (n < 2){
+		return 1;
+	}
+	else{
+		return fib(n - 1) + fib(n - 2);
+	}
+}
+int main_0(){
 	int input = 0;
+	int n = 2;
 	scanf("%d", &input);
 	while (1){
+		if (input > fib(n - 1) && input <= fib(n)){
+			printf("%d\n", input - fib(n - 1) > fib(n) - input ? fib(n) - input : input - fib(n - 1));
+			break;
+		}
+		else {
+			n++;
+		}
+	}
+	return 0;
+}
 
+int main() {
+	int num = 0;
+	scanf("%d", &num);
+	int f1 = 0, f2 = 1, f3 = f1 + f2;
+	while (1)
+	{
+		if (f2 == num)
+		{
+			printf("%d\n", 0);
+			break;
+		}
+		else if (f2 > num)
+		{
+			if (abs(f2 - num) > abs(f1 - num))
+			{
+				printf("%d\n", abs(f1 - num));
+			}
+			else
+			{
+				printf("%d\n", abs(f2 - num));
+			}
+			break;
+		}
+		f1 = f2;
+		f2 = f3;
+		f3 = f1 + f2;
 	}
 	return 0;
 }
