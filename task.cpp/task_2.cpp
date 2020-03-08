f383a7a746981cbae0a4efbe0a31fcc94ba909a6
武纪东 <Angelsufeiya@hotmail.com>
2020-03-08 19:12:27 +0800

    come on

--- task.cpp/task_2.cpp
+++ task.cpp/task_2.cpp
@@ -4,11 +4,28 @@ using namespace std;
 //对字符串进行RLE压缩，将相邻的相同字符，用计数值和字符值来代替。
 //例如：aaabccccccddeee，则可用3a1b6c2d3e来代替。
 
+void Compress(char *str) {
+	int count = 1;
+	while (*str) {
+		if (*str == *(str + 1)) {
+			++count;
+			str++;
+		}
+		else {
+			cout << count << *str;
+			count = 1;
+			str++;
+		}
+	}
+}
+
+
 int main_2() {
 	char array[1000] = { 0 };
 
+	cout << "请输入您要压缩的字符串：";
 	cin >> array;
-
+	Compress(array);
 
 	return 0;
 }
--- task.cpp/task_3.cpp
+++ task.cpp/task_3.cpp
@@ -3,10 +3,45 @@ using namespace std;
 
 //删除字符串首尾空格，中间的连续空格只留一个，原来字符串的顺序不变。
 
-int main_3() {
-	char array = { 0 };
+void Deblank(char * str) {
+	int flag = 0;	//代表没开始处理空格
+	int p = 0;
+	int i = 0;
+	while (str[i]) {
+		//遇到空格 还没开始处理
+		if (!flag && str[i] == ' ') {
+			i++;
+		}
+		//遇到不是空格 还没开始处理
+		else if (!flag && str[i] != ' ') {
+			flag = 1;
+			str[p++] = str[i++];
+		}
+		//遇到空格 开始处理了
+		else if (flag && str[i] == ' ') {
+			flag = 0;
+			str[p++] = str[i++];
+		}
+		//遇到不是空格 开始处理了
+		else //if(flag && str[i] != ' ')
+		{
+			flag = 1;
+			str[p++] = str[i++];
+		}
+	}
 
-	cin >> array;
+	if (str[p - 1] == ' ') {
+		str[p - 1] = '\0';
+	}
+	else
+		str[p] = '\0';
+}
+
+int main() {
+	char array[] = "  asadad   q    ";
+
+	Deblank(array);
+	cout << array << endl;
 
 	return 0;
 }
