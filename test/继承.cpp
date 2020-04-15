6a1a525ecbd7e31759faa6015654494d3234ccc9
武纪东 <Angelsufeiya@hotmail.com>
2020-04-15 18:51:35 +0800

    come on

--- test/继承.cpp
+++ test/继承.cpp
@@ -256,45 +256,304 @@ using namespace std;
 //    return 0;
 //}
 
-class Person
-{
+//class Person
+//{
+//public:
+//    Person() 
+//    { 
+//        ++_count; 
+//    }
+//
+//    static int _count; // 统计人的个数。 
+//protected:
+//    string _name; // 姓名 
+//};
+//
+//int Person::_count = 0;
+//
+//class Student : public Person
+//{
+//protected:
+//    int _stuNum; // 学号 
+//};
+//
+//class Graduate : public Student
+//{
+//protected:
+//    string _seminarCourse; // 研究科目 
+//};
+//
+//int main()
+//{
+//    Student s1;
+//    Student s2;
+//    Student s3;
+//    Graduate s4;
+//    cout << " 人数 :" << Person::_count << endl; 
+//    Student::_count = 0;
+//    cout << " 人数 :" << Person::_count << endl;
+//
+//    return 0;
+//}
+
+// 人数 :4
+// 人数 :0
+
+//class Person
+//{
+//public:
+//    string _name; // 姓名 
+//};
+//
+//class Student : public Person
+//{
+//protected:
+//    int _num; //学号 
+//};
+//class Teacher : public Person
+//{
+//protected:
+//    int _id; // 职工编号 
+//};
+//
+//class Assistant : public Student, public Teacher
+//{
+//protected:
+//    string _majorCourse; // 主修课程 
+//};
+//
+//void Test() {
+//    // 这样会有二义性无法明确知道访问的是哪一个 
+//    Assistant a ;
+//    a._name = "peter";
+//    // 需要显示指定访问哪个父类的成员可以解决二义性问题，但是数据冗余问题无法解决 
+//    a.Student::_name = "xxx";
+//    a.Teacher::_name = "yyy";
+//}
+
+
+//class Person
+//{
+//public:
+//    string _name; // 姓名 
+//};
+//
+//class Student : virtual public Person
+//{
+//protected:
+//    int _num; //学号 
+//};
+//class Teacher : virtual public Person
+//{
+//protected:
+//    int _id; // 职工编号 
+//};
+//
+//class Assistant : public Student, public Teacher
+//{
+//protected:
+//    string _majorCourse; // 主修课程 
+//};
+//
+//void Test() {
+//    Assistant a;
+//    a._name = "peter";
+//}
+
+//class A
+//{
+//public:
+//    int _a;
+//};
+//
+//// class B : public A
+//class B : virtual public A
+//{
+//public:
+//    int _b;
+//};
+//// class C : public A
+//class C : virtual public A
+//{
+//public:
+//    int _c;
+//};
+//
+//class D : public B, public C
+//{
+//public:
+//    int _d;
+//};
+//
+//int main() {
+//    D d;
+//    d.B::_a = 1;
+//    d.C::_a = 2;
+//    d._b = 3;
+//    d._c = 4;
+//    d._d = 5;
+//
+//    return 0;
+//}
+//
+//// 有童鞋会有疑问为什么D中B和C部分要去找属于自己的A?
+//// 那么大家看看当下面的赋值发生时，d是不是要 去找出B/C成员中的A才能赋值过去?
+//D d;
+//B b = d;
+//C c = d;
+
+
+//// Car和BMW Car和Benz构成is-a的关系 
+//class Car{
+//protected:
+//    string _colour = "白色"; // 颜色
+//    string _num = "陕ABIT00"; // 车牌号 
+//};
+//
+//class BMW : public Car {
+//public:
+//    void Drive() { cout << "好开-操控" << endl; }
+//};
+//class Benz : public Car {
+//public:
+//    void Drive() { cout << "好坐-舒适" << endl; }
+//};
+//
+//// Tire和Car构成has-a的关系
+//class Tire {
+//protected:
+//    string _brand = "Michelin"; // 品牌
+//    size_t _size = 17;  // 尺寸
+//};
+//
+//class Car {
+//protected:
+//    string _colour = "白色";  // 颜色
+//    string _num = "陕ABIT00"; // 车牌号
+//    Tire _t;    // 轮胎
+//};
+
+//class Base1 {
+//public:
+//	int _b1;
+//};
+//
+//class Base2 {
+//public:
+//    int _b2;
+//};
+//
+//class Derive : public Base1, public Base2 {
+//public:
+//    int _d;
+//};
+//
+//int main() {
+//    // A. p1 == p2 == p3
+//    // B. p1 < p2 < p3
+//    // C. p1 == p3 != p2
+//    // D. p1 != p2 != p3
+//    Derive d;
+//    Base1* p1 = &d;
+//    Base2* p2 = &d;
+//    Derive* p3 = &d;
+//    return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//
+//class TestBase
+//{
+//public:
+//	virtual void func()
+//	{
+//		cout << "I'm TestBase!" << endl;
+//	}
+//};
+//
+//class TestBase2 : public TestBase
+//{
+//public:
+//	void func()
+//	{
+//		cout << "I'm TestBase2!" << endl;
+//	}
+//};
+//
+//class TestBase3 : public TestBase2
+//{
+//public:
+//	void func()
+//	{
+//		cout << "I'm TestBase3!" << endl;
+//	}
+//};
+//
+//int main()
+//{
+//	/*
+//	TestBase tb;
+//	Test t;
+//	TestBase * ptb = &t;
+//	ptb->func();
+//	tb.func();
+//	t.func();*/
+//
+//#if 0
+//	TestBase3 t3;
+//	TestBase2* pt2 = &t3;
+//	TestBase* pt = pt2;
+//
+//	t3.func();
+//	pt2->func();
+//	pt->func();
+//#else
+//	TestBase2 t2;
+//	TestBase* pt = &t2;
+//	t2.func();
+//	pt->func();
+//#endif
+//	return 0;
+//}
+
+class Base1 {
 public:
-    Person() 
-    { 
-        ++_count; 
-    }
-
-    static int _count; // 统计人的个数。 
-protected:
-    string _name; // 姓名 
+    int _b1;
 };
 
-int Person::_count = 0;
-
-class Student : public Person
-{
-protected:
-    int _stuNum; // 学号 
+class Base2 {
+public:
+    int _b2;
 };
 
-class Graduate : public Student
-{
-protected:
-    string _seminarCourse; // 研究科目 
+class Derive : public Base1, public Base2 {
+public:
+    int _d;
 };
 
-int main()
-{
-    Student s1;
-    Student s2;
-    Student s3;
-    Graduate s4;
-    cout << " 人数 :" << Person::_count << endl; 
-    Student::_count = 0;
-    cout << " 人数 :" << Person::_count << endl;
+int main() {
+    Derive d;
+    Base1* p1 = &d;
+    Base2* p2 = &d;
+    Derive* p3 = &d;
+
+    // 地址的输出不一定每次相同，但是 p1 == p3 != p2 不变
+    cout << p1 << endl; // 00AFFC54
+    cout << p2 << endl; // 00AFFC58
+    cout << p3 << endl; // 00AFFC54
+
+    if (p2 == p3)
+        cout << "p2 == p3" << endl; // p2 == p3
+    else
+        cout << "p2 != p3" << endl;
 
     return 0;
 }
 
-// 人数 :4
-// 人数 :0
+// A. p1 == p2 == p3
+// B. p1 < p2 < p3
+// C. p1 == p3 != p2
+// D. p1 != p2 != p3
+
+// 答案为：C
