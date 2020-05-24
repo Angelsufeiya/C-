05ee13fd877ce1584d019219c09db341f714bf55
武纪东 <Angelsufeiya@hotmail.com>
2020-05-24 23:14:09 +0800

    come on

--- 每日一题-1.cpp/day_14.cpp
+++ 每日一题-1.cpp/day_14.cpp
@@ -28,7 +28,42 @@
 //		输出
 //		366
 //
-//
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int iConverDateToDay(int year, int month, int day)
+{
+    if (month > 12 || month < 0) {
+        return -1;
+    }
+
+    vector<int> v = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    if (year < 0) {
+        year = 0 - year;
+    }
+    if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)) {
+        v[2] = 29;
+    }
+
+    if (day > v[month] || day < 0) {
+        return -1;
+    }
+
+    int sum = 0;
+    for (int i = 1; i < month; i++) {
+        sum += v[i];
+    }
+    return sum + day;
+}
+
+int main_15() {
+    int year, month, day;
+    while (cin >> year >> month >> day) {
+        cout << iConverDateToDay(year, month, day) << endl;
+    }
+    return 0;
+}
 //
 //	标题 : 幸运的袋子
 //		 一个袋子里面有n个球，每个球上面都有一个号码(拥有相同号码的球是无区别的)。如果一个袋子是幸运的当 且仅当所有球的号码的和大于所有球的号码的积。
@@ -42,3 +77,65 @@
 //	   1 1 1
 //	   输出
 //	   2
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+/*
+    getLuckyPacket:从当前位置开始搜索符合要求的组合，一直搜索到最后一个位置结束 
+    v: 袋子中的所有球
+    n: 球的总数
+    pos: 当前搜索的位置
+    sum: 到目前位置的累加和
+    mul: 到目前位置的累积值 
+*/
+int getLuckyPacket(vector<int> v, int n, int pos, int sum, int mul) {
+    int count = 0; 
+    //循环，搜索以位置i开始所有可能的组合 
+    for (int i = pos; i<n; i++)
+    {
+        sum += v[i];
+        mul *= v[i];
+        if (sum > mul)
+        {
+            //找到符合要求的组合，加1，继续累加后续的值，看是否有符合要求的集合
+            count += 1 + getLuckyPacket(v, n, i + 1, sum, mul);
+        }
+        else if (v[i] == 1)
+        {
+            //如何不符合要求，且当前元素值为1，则继续向后搜索
+            count += getLuckyPacket(v, n, i + 1, sum, mul);
+        }
+        else
+        {
+            //如何sum大于multi,则后面就没有符合要求的组合了
+            break;
+        }
+        //要搜索下一个位置之前，首先恢复sum和multi
+        sum -= v[i];
+        mul /= v[i]; 
+        //数字相同的球，没有什么区别，都只能算一个组合，所以直接跳过 
+        while (i < n - 1 && v[i] == v[i + 1])
+        {
+            i++;
+        } 
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> v;
+    v.resize(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+
+    cout << getLuckyPacket(v, n, 0, 0, 1) << endl;
+
+    return 0;
+}
