bc46f7852a8da2287a5b53dfdb6aba57c31dceb9
武纪东 <Angelsufeiya@hotmail.com>
2020-08-05 22:43:02 +0800

    come on

--- 每日一题-2.cpp/day_20.cpp
+++ 每日一题-2.cpp/day_20.cpp
@@ -1,18 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
+// ¹«¹²×Ö´®¼ÆËã
+#include <iostream>
+#include <vector>
+using namespace std;
 
-int change(int *px){
-	int y = 8;
-	//y = y - *px;
-	px = &y;
-	return 0;
+int main() {
+    string s1, s2;
+    while (cin >> s1 >> s2) {
+        int len1 = s1.size();
+        int len2 = s2.size();
+
+        int max = 0;
+        vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1, 0));
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    if (dp[i][j] > max) {
+                        max = dp[i][j];
+                    }
+                }
+            }
+        }
+        cout << max << endl;
+    }
+
+    return 0;
 }
 
-int main(){
-	int xx = 3;
-	int *pY = &xx;
-	change(pY);
-	printf("%d", *pY);
-	system("pause");
-	return 0;
-}
